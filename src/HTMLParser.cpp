f1eb14477d665375a0223ec64d132d76b730fccf
Benjamin Zou <bwz5@cornell.edu>
2024-03-25 16:31:01 -0400

    testing enumeration

--- src/HTMLParser.cpp
+++ src/HTMLParser.cpp
@@ -3,10 +3,8 @@
 #include <iostream>
 #include <sstream>
 
-std::string HTMLParser::createHTMLString(std::string fileName) {
+void HTMLParser::createHTMLString(std::string fileName) {
     std::ifstream htmlTextFile(fileName, std::ios::in);
-
-    std::string output = "";
 
     if (htmlTextFile.is_open()){
         std::string line;
@@ -16,26 +14,251 @@ std::string HTMLParser::createHTMLString(std::string fileName) {
             std::stringstream ss(token);
 
             while (getline(ss, token, ' ')) {
-                output += token;
+                HTMLString.push_back(token);
             }
         }
     }
-    if (output == ""){
+    if (HTMLString.empty()){
         std::cout << "The returned HTML String was empty." << std::endl;
     } else {
         std::cout << "The HTML String has been made" << std::endl;
     }
-    return output;
 }
 
-int HTMLParser::parseHTMLString(std::string HTMLString) {
+void HTMLParser::parseHTMLString() { //TODO: Either split with '=' to get attributes or use < to get starting tag and </ to get end
     HTMLTree* current = root;
 
-    
+    for (auto token : HTMLString){
+        if (token.size() >= 2){
+            if (token[0] == '<' && token[1] != '/'){ // Goes back to the parent
+                std::string tokenTag = "";
+                for (int i = 2; i < token.size()-1; i++){
+                    tokenTag += token[i];
+                }
+                switch(htmlMap[tokenTag]){
+                    case a:
+                        std::cout << "saw an a tag \n";
+                    case html: 
+                    case abbr: 
+                    case address: 
+                    case area: 
+                    case article: 
+                    case aside: 
+                    case audio: 
+                    case b: 
+                    case bdi: 
+                    case bdo: 
+                    case blockquote: 
+                    case body: 
+                    case br: 
+                    case button: 
+                    case canvas: 
+                    case caption: 
+                    case cite: 
+                    case code: 
+                    case col: 
+                    case colgroup: 
+                    case data: 
+                    case datalist: 
+                    case dd: 
+                    case del: 
+                    case details: 
+                    case dfn: 
+                    case dialog: 
+                    case div: 
+                    case dl: 
+                    case dt: 
+                    case em: 
+                    case embed: 
+                    case fieldset: 
+                    case figcaption: 
+                    case figure: 
+                    case footer: 
+                    case form: 
+                    case h: 
+                    case head: 
+                    case header: 
+                    case  hr: 
+                    case  i: 
+                    case iframe: 
+                    case img: 
+                    case input: 
+                    case ins: 
+                    case kbd: 
+                    case label: 
+                    case legend: 
+                    case li: 
+                    case link: 
+                    case main: 
+                    case map: 
+                    case mark: 
+                    case meta: 
+                    case meter: 
+                    case nav: 
+                    case noscript: 
+                    case object: 
+                    case ol: 
+                    case optgroup: 
+                    case option: 
+                    case output: 
+                    case p: 
+                    case param: 
+                    case picture: 
+                    case pre: 
+                    case progress: 
+                    case q: 
+                    case rp: 
+                    case rt: 
+                    case ruby: 
+                    case s: 
+                    case samp: 
+                    case script: 
+                    case section: 
+                    case slot: 
+                    case small: 
+                    case source: 
+                    case span: 
+                    case strong: 
+                    case style: 
+                    case sub: 
+                    case summary: 
+                    case sup: 
+                    case svg: 
+                    case table: 
+                    case tbody: 
+                    case td:
+                    case templateTag:
+                    case textarea:
+                    case tfoot:
+                    case th:
+                    case thead:
+                    case timeTag:
+                    case title:
+                    case tr:
+                    case track:
+                    case u:
+                    case ul:
+                    case var:
+                    case video:
+                    case wbr: 
+                    default:
+                }
+
+            }
+            else if (token[0] == '<' && token[1] == '/'){ // Goes back to the parent
+                current = current->parent;
+            }
+        }
+    }
 }
 
 HTMLParser::HTMLParser() {
-    root->parent = NULL;
+    root->parent = nullptr;
+
+    htmlMap["a"] = a; // TODO: Finish this
+    htmlMap["html"] = html;
+    htmlMap["abbr"] = abbr;
+    htmlMap["address"] = address;
+    htmlMap["area"] = area;
+    htmlMap["article"] = article;
+    htmlMap["aside"] = aside;
+    htmlMap["audio"] = audio;
+    htmlMap["b"] = b;
+    htmlMap["bdi"] = bdi;
+    htmlMap["bdo"] = bdo;
+    htmlMap["blockquote"] = blockquote;
+    htmlMap["body"] = body;
+    htmlMap["br"] = br;
+    htmlMap["button"] = button;
+    htmlMap["canvas"] = canvas;
+    htmlMap["caption"] = caption;
+    htmlMap["cite"] = cite;
+    htmlMap["code"] = code;
+    htmlMap["col"] = col;
+    htmlMap["colgroup"] = colgroup;
+    htmlMap["data"] = data;
+    htmlMap["datalist"] = datalist;
+    htmlMap["dd"] = dd;
+    htmlMap["del"] = del;
+    htmlMap["details"] = details;
+    htmlMap["dfn"] = dfn;
+    htmlMap["dialog"] = dialog;
+    htmlMap["div"] = div;
+    htmlMap["dl"] = dl;
+    htmlMap["dt"] = dt;
+    htmlMap["em"] = em;
+    htmlMap["embed"] = embed;
+    htmlMap["fieldset"] = fieldset;
+    htmlMap["figcaption"] = figcaption;
+    htmlMap["figure"] = figure;
+    htmlMap["footer"] = footer;
+    htmlMap["form"] = form;
+    htmlMap["h"] = h;
+    htmlMap["head"] = head;
+    htmlMap["header"] = header;
+    htmlMap["hr"] = hr;
+    htmlMap["i"] = i;
+    htmlMap["iframe"] = iframe;
+    htmlMap["img"] = img;
+    htmlMap["input"] = input;
+    htmlMap["ins"] = ins;
+    htmlMap["kbd"] = kbd;
+    htmlMap["label"] = label;
+    htmlMap["legend"] = legend;
+    htmlMap["li"] = li;
+    htmlMap["link"] = link ;
+    htmlMap["main"] = main;
+    htmlMap["map"] = map;
+    htmlMap["mark"] = mark;
+    htmlMap["meta"] = meta;
+    htmlMap["meter"] = meter;
+    htmlMap["nav"] = nav;
+    htmlMap["noscript"] = noscript;
+    htmlMap["object"] = object;
+    htmlMap["ol"] = ol;
+    htmlMap["optgroup"] = optgroup;
+    htmlMap["option"] = option;
+    htmlMap["output"] = output;
+    htmlMap["p"] = p;
+    htmlMap["param"] = param;
+    htmlMap["picture"] = picture;
+    htmlMap["pre"] = pre;
+    htmlMap["progress"] = progress;
+    htmlMap["q"] =  q;
+    htmlMap["rp"] = rp;
+    htmlMap["rt"] = rt;
+    htmlMap["ruby"] = ruby;
+    htmlMap["s"] = s;
+    htmlMap["samp"] = samp;
+    htmlMap["script"] = script;
+    htmlMap["section"] = section;
+    htmlMap["slot"] = slot;
+    htmlMap["small"] = small;
+    htmlMap["source"] = source;
+    htmlMap["span"] = span;
+    htmlMap["strong"] = strong;
+    htmlMap["style"] = style;
+    htmlMap["sub"] = sub;
+    htmlMap["summary"] = summary;
+    htmlMap["sup"] = sup;
+    htmlMap["svg"] = svg;
+    htmlMap["table"] = table;
+    htmlMap["tbody"] = tbody;
+    htmlMap["td"] = td;
+    htmlMap["templateTag"] = templateTag;
+    htmlMap["textarea"] = textarea;
+    htmlMap["tfoot"] = tfoot;
+    htmlMap["th"] = th;
+    htmlMap["thead"] = thead;
+    htmlMap["timeTag"] = timeTag;
+    htmlMap["title"] = title;
+    htmlMap["tr"] = tr;
+    htmlMap["trac"] = track;
+    htmlMap["u"] = u;
+    htmlMap["ul"] = ul;
+    htmlMap["var"] = var;
+    htmlMap["video"] = video;
+    htmlMap["wbr"] = wbr;
 }
 
-HTMLParser::~HTMLParser() {}
+HTMLParser::~HTMLParser() = default;
--- src/HTMLParser.h
+++ src/HTMLParser.h
@@ -1,15 +1,128 @@
 #include "HTMLTree.h"
 #include <string>
+#include <vector>
+#include <map>
 
 class HTMLParser {
 private:
     HTMLTree* root;
+    std::vector<std::string> HTMLString;
+
+    enum HTMLTags {
+        a,
+        html,
+        abbr,
+        address,
+        area,
+        article,
+        aside,
+        audio,
+        b,
+        bdi,
+        bdo,
+        blockquote,
+        body,
+        br,
+        button,
+        canvas,
+        caption,
+        cite,
+        code,
+        col,
+        colgroup,
+        data,
+        datalist,
+        dd,
+        del,
+        details,
+        dfn,
+        dialog,
+        div,
+        dl,
+        dt,
+        em,
+        embed,
+        fieldset,
+        figcaption,
+        figure,
+        footer,
+        form,
+        h,
+        head,
+        header,
+        hr,
+        i,
+        iframe,
+        img,
+        input,
+        ins,
+        kbd,
+        label,
+        legend,
+        li,
+        link,
+        main,
+        map,
+        mark,
+        meta,
+        meter,
+        nav,
+        noscript,
+        object,
+        ol,
+        optgroup,
+        option,
+        output,
+        p,
+        param,
+        picture,
+        pre,
+        progress,
+        q,
+        rp,
+        rt,
+        ruby,
+        s,
+        samp,
+        script,
+        section,
+        slot,
+        small,
+        source,
+        span,
+        strong,
+        style,
+        sub,
+        summary,
+        sup,
+        svg,
+        table,
+        tbody,
+        td,
+        templateTag,
+        textarea,
+        tfoot,
+        th,
+        thead,
+        timeTag,
+        title,
+        tr,
+        track,
+        u,
+        ul,
+        var,
+        video,
+        wbr
+    };
+
+    std::map<std::string , HTMLTags> htmlMap;
 public:
+
     HTMLParser();
 
     ~HTMLParser();
 
-    int parseHTMLString(std::string HTMLString);
+    void parseHTMLString();
 
-    std::string createHTMLString(std::string fileName);
+    void createHTMLString(std::string fileName);
 };
